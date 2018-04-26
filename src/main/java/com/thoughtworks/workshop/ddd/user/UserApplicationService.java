5f90b26a0584b529f1d5d1a00e2d76dfeb223871
Waterstrong <sqlin@thoughtworks.com>
2018-04-26 19:57:00 +0800

    [Shuiqiang] - update the user application service

--- src/main/java/com/thoughtworks/workshop/ddd/user/UserApplicationService.java
+++ src/main/java/com/thoughtworks/workshop/ddd/user/UserApplicationService.java
@@ -1,34 +1,41 @@
 package com.thoughtworks.workshop.ddd.user;
 
-import com.thoughtworks.workshop.ddd.persistence.user.UserRepositoryInMemory;
-import com.thoughtworks.workshop.ddd.user.command.InitPasswordCommand;
-import com.thoughtworks.workshop.ddd.user.command.LoginCommand;
-import com.thoughtworks.workshop.ddd.user.command.RegisterCommand;
-import com.thoughtworks.workshop.ddd.user.domain.model.User;
-import com.thoughtworks.workshop.ddd.user.domain.service.LoginService;
-import com.thoughtworks.workshop.ddd.user.domain.service.RegisterService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import com.thoughtworks.workshop.ddd.user.command.InitPasswordCommand;
+import com.thoughtworks.workshop.ddd.user.command.LoginCommand;
+import com.thoughtworks.workshop.ddd.user.command.RegisterCommand;
+import com.thoughtworks.workshop.ddd.user.domain.model.User;
+import com.thoughtworks.workshop.ddd.user.domain.service.EmailService;
+import com.thoughtworks.workshop.ddd.user.domain.service.LoginService;
+import com.thoughtworks.workshop.ddd.user.domain.service.RegisterService;
+import com.thoughtworks.workshop.ddd.user.repository.UserRepository;
 
 @Service
 public class UserApplicationService {
     private static Logger logger = LoggerFactory.getLogger(UserApplicationService.class);
 
     @Autowired
-    private UserRepositoryInMemory userRepository;
+    private UserRepository userRepository;
 
     @Autowired
     private LoginService loginService;
 
     @Autowired
-    private RegisterService registerService;
+    EmailService emailService;
 
+    @Autowired
+    private RegisterService registerService;
+    
     public String register(RegisterCommand command) {
         User user = registerService.createUser(command.getOwnerEmail(), command.getPolicyNumber());
+        String uuid = user.getUuid();
+        userRepository.save(user);
+        emailService.sendEmail(uuid);
         logger.info("Create user with email [{}]", command.getOwnerEmail());
-        return user.getUuid();
+        return uuid;
     }
 
     public void initPassword(InitPasswordCommand command) {
--- src/main/java/com/thoughtworks/workshop/ddd/user/domain/service/RegisterService.java
+++ src/main/java/com/thoughtworks/workshop/ddd/user/domain/service/RegisterService.java
@@ -1,30 +1,25 @@
 package com.thoughtworks.workshop.ddd.user.domain.service;
 
-import com.thoughtworks.workshop.ddd.exception.BadRequestException;
-import com.thoughtworks.workshop.ddd.exception.ResourceConflictException;
-import com.thoughtworks.workshop.ddd.persistence.user.UserRepositoryInMemory;
-import com.thoughtworks.workshop.ddd.user.domain.model.User;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.UUID;
+import com.thoughtworks.workshop.ddd.exception.BadRequestException;
+import com.thoughtworks.workshop.ddd.exception.ResourceConflictException;
+import com.thoughtworks.workshop.ddd.user.domain.model.User;
+import com.thoughtworks.workshop.ddd.user.repository.UserRepository;
 
 @Service
 public class RegisterService {
 
     @Autowired
-    private UserRepositoryInMemory userRepository;
-
-    @Autowired
-    EmailService emailService;
+    private UserRepository userRepository;
 
     public User createUser(String email, String policyNumber) {
         if (userRepository.existByEmail(email)) throw new ResourceConflictException("Email exist");
         if (!validate(email, policyNumber)) throw new BadRequestException("PolicyNumber not correct");
         String uuid = UUID.randomUUID().toString();
         User user = new User(uuid, null, email);
-        userRepository.save(user);
-        emailService.sendEmail(uuid);
         return user;
     }
 
